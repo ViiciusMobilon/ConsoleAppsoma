d2808f54d0f1c67c6e25d066a6a7dffb4bcb4ead
Usuario <Usuario@CPD02-M13>
2024-02-27 10:53:57 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppsoma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppsoma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Digite o primeiro numero");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int resultado = n1 * n2;
+
+            Console.WriteLine("O resultado é:" + resultado);
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
